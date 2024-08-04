c7862dc34c0613f9710014b64dcbe4311fec8848
feroxfoxxo@gmail.com <feroxfoxxo@gmail.com>
2024-08-04 13:57:36 +1200

    fix: removed references to entity framework

--- Server.Web/Models/MessageModel.cs
+++ Server.Web/Models/MessageModel.cs
@@ -5,6 +5,12 @@ using System.Text.Json.Serialization;
 namespace Server.Web.Models;
 public class MessageModel
 {
+    public string Message { get; set; }
+    public string ColorHex { get => HexConverter(InternalColor); set => InternalColor = Color.FromArgb(int.Parse(value.Replace("#", ""), NumberStyles.HexNumber)); }
+
+    [JsonIgnore]
+    public Color InternalColor { get; set; }
+
     public MessageModel() { }
 
     public MessageModel(string message, Color color)
@@ -12,12 +18,6 @@ public class MessageModel
         Message = message;
         InternalColor = color;
     }
-
-    public string Message { get; set; }
-    public string ColorHex { get => HexConverter(InternalColor); set => InternalColor = Color.FromArgb(int.Parse(value.Replace("#", ""), NumberStyles.HexNumber)); }
-
-    [JsonIgnore]
-    public Color InternalColor { get; set; }
 
     private static string HexConverter(Color c) =>
         $"#{c.R:X2}{c.G:X2}{c.B:X2}";
