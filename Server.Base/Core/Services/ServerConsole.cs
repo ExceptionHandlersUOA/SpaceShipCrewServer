14031782178eb94b880548865b111a63d021336c
feroxfoxxo@gmail.com <feroxfoxxo@gmail.com>
2024-08-09 16:56:45 +1200

    Use console thread for launching new command

--- Server.Base/Core/Services/ServerConsole.cs
+++ Server.Base/Core/Services/ServerConsole.cs
@@ -5,13 +5,35 @@ using Server.Base.Core.Models;
 using Server.Base.Timers.Extensions;
 using Server.Base.Timers.Services;
 using Server.Base.Worlds;
+using System.Globalization;
 using static Server.Base.Core.Models.ConsoleCommand;
 
 namespace Server.Base.Core.Services;
 
-public class ServerConsole(TimerThread timerThread, ServerHandler handler, ILogger<ServerConsole> logger, World world, EventSink sink) : IService
+public class ServerConsole : IService
 {
     private readonly Dictionary<string, ConsoleCommand> _commands = [];
+    private readonly TimerThread timerThread;
+    private readonly ServerHandler handler;
+    private readonly ILogger<ServerConsole> logger;
+    private readonly World world;
+    private readonly EventSink sink;
+    private readonly Thread _consoleThread;
+
+    public ServerConsole(TimerThread timerThread, ServerHandler handler, ILogger<ServerConsole> logger, World world, EventSink sink)
+    {
+        this.timerThread = timerThread;
+        this.handler = handler;
+        this.logger = logger;
+        this.world = world;
+        this.sink = sink;
+
+        _consoleThread = new Thread(ConsoleLoopThread)
+        {
+            Name = "Console Thread",
+            CurrentCulture = CultureInfo.InvariantCulture
+        };
+    }
 
     public void Initialize() => sink.ServerStarted += (_) => RunConsoleListener();
 
@@ -39,7 +61,7 @@ public class ServerConsole(TimerThread timerThread, ServerHandler handler, ILogg
 
         DisplayHelp();
 
-        timerThread.DelayCall((_) => ProcessCommand(Console.ReadLine()), null, TimeSpan.FromSeconds(0), TimeSpan.FromMilliseconds(50), 0);
+        _consoleThread.Start();
     }
 
     public void AddCommand(string name, string description, RunConsoleCommand commandMethod, bool strictCheck = false) =>
@@ -74,10 +96,15 @@ public class ServerConsole(TimerThread timerThread, ServerHandler handler, ILogg
             var inputs = input.Trim().Split();
             var name = inputs.FirstOrDefault();
 
-            if (name != null && _commands.TryGetValue(name, out var value))
+            if (name != null)
             {
-                value.CommandMethod(inputs);
-                logger.LogInformation("Successfully ran command '{Name}'", name);
+                if (_commands.TryGetValue(name, out var value))
+                {
+                    value.CommandMethod(inputs);
+                    logger.LogInformation("Successfully ran command '{Name}'", name);
+                }
+                else
+                    DisplayHelp();
             }
         }
     }
